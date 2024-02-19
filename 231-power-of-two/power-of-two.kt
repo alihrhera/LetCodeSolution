7070618b3f00bab89f7b2db507339d8f607dfe0c
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-02-19 23:09:38 +0200

    Time: 147 ms (57.39%) | Memory: 33.5 MB (74.78%) - LeetSync

--- 231-power-of-two/power-of-two.kt
+++ 231-power-of-two/power-of-two.kt
@@ -2,7 +2,7 @@ class Solution {
 
     fun isPowerOfTwo(n: Int): Boolean {
         if (n == 0) return false
-        if(n == 1) return true
+        if(n == 1||n==2) return true
         if(n %2  != 0) return false
         return n>0&&(n and (n - 1)) == 0
     }

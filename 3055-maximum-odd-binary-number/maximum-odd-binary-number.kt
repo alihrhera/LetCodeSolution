00533b12ee406a0104f85d488f8eb0d520af30a4
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-03-01 23:44:31 +0200

    Time: 193 ms (45.45%) | Memory: 37.7 MB (36.36%) - LeetSync

---
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.kt
@@ -0,0 +1,32 @@
+class Solution {
+     fun maximumOddBinaryNumber(s: String): String {
+        if (!s.all { it == '1' || it == '0' }) throw Exception("Only 1 and 0 accepted")
+        if (s.length == 1) return s
+        val sChar = s.toCharArray()
+        var lastItemIs1 = sChar.last() == '1'
+        var index = if (lastItemIs1) sChar.lastIndex - 1 else sChar.lastIndex
+        while (index >= 0) {
+            if (!lastItemIs1) {
+                swap(sChar,sChar.indexOfLast { it=='1' },sChar.lastIndex)
+                lastItemIs1 = sChar.last() == '1'
+            } else {
+                if (sChar[index] == '1') {
+                    var i = index
+                    while (i > -1) {
+                        if (sChar[i] == '0') break
+                        i--
+                    }
+                    if (i in sChar.indices) swap(sChar, index, i)
+                }
+            }
+            index--
+        }
+        return String(sChar)
+    }
+
+    private fun swap(chars: CharArray, x: Int, y: Int) {
+        val temp = chars[x]
+        chars[x] = chars[y]
+        chars[y] = temp
+    }
+}

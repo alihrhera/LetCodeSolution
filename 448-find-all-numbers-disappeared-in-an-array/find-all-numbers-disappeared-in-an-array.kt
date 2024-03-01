ae378b476b8a6a1a5b0da5f0a3d47ad7b7620ab5
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-03-02 01:41:50 +0200

    Time: 422 ms (42.94%) | Memory: 51.7 MB (11.18%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.kt
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.kt
@@ -1,12 +1,17 @@
 class Solution {
      fun findDisappearedNumbers(nums: IntArray): List<Int> {
-        val outPutList = mutableListOf<Int>()
+         val outPutList = mutableListOf<Int>()
+
+        val numsHashSet = nums.toHashSet()
         var start = 1
         var end = nums.size
-
         while (start < end) {
-            if (start !in nums) outPutList.add(start)
-            if (end   !in nums) outPutList.add(end)
+            if (!numsHashSet.contains(start)) {
+                outPutList.add(start)
+            }
+            if (!numsHashSet.contains(end)) {
+                outPutList.add(end)
+            }
             start++
             end--
         }

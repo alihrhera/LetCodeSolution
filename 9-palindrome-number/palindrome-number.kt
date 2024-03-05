3cf6a9b97f89fe0e22a855b29612f807ad1fdc73
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-03-05 09:59:32 +0200

    Time: 210 ms (73.76%) | Memory: 37.8 MB (21.73%) - LeetSync

--- 9-palindrome-number/palindrome-number.kt
+++ 9-palindrome-number/palindrome-number.kt
@@ -1,7 +1,23 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        if (x < 0) return false
-        if (x < 10) return true
-        return "$x" == "$x".reversed()
+        if (x<0) return false
+        if (x<10)return true
+        var s=0
+        var e="$x".lastIndex
+
+        while(s<e){
+            if ("$x"[s]!="$x"[e]){
+                return false 
+            }
+            s++
+            e--
+        }
+
+
+        return true
+
+
+       
+        
     }
 }

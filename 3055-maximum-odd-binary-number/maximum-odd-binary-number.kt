39de2a21ab05e998885aadf699b7ae3e0a3fbd2d
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-03-07 10:43:56 +0200

    Time: 205 ms (27.06%) | Memory: 38 MB (12.09%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.kt
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.kt
@@ -1,29 +1,14 @@
 class Solution {
      fun maximumOddBinaryNumber(s: String): String {
-        if (!s.all { it == '1' || it == '0' }) throw Exception("Only 1 and 0 accepted")
         if (s.length == 1) return s
         val sChar = s.toCharArray()
-        if (s.count { it == '1' } == 1) {
-            swap(sChar, sChar.lastIndex, sChar.indexOfFirst { it == '1' })
-            return String(sChar)
-        }
-        var lastItemIs1 = sChar.last() == '1'
-        var index = if (lastItemIs1) sChar.lastIndex - 1 else sChar.lastIndex
-        while (index >= 0) {
-            if (!lastItemIs1) {
-                swap(sChar,sChar.indexOfLast { it=='1' },sChar.lastIndex)
-                lastItemIs1 = sChar.last() == '1'
-            } else {
-                if (sChar[index] == '1') {
-                    var i = index
-                    while (i > -1) {
-                        if (sChar[i] == '0') break
-                        i--
-                    }
-                    if (i in sChar.indices) swap(sChar, index, i)
-                }
+        Arrays.sort(sChar)
+        sChar.reverse()
+        for (i in sChar.lastIndex downTo 0) {
+            if (sChar[i] == '1') {
+                swap(sChar, i, sChar.lastIndex)
+                break
             }
-            index--
         }
         return String(sChar)
     }
@@ -33,5 +18,4 @@ class Solution {
         chars[x] = chars[y]
         chars[y] = temp
     }
-    
 }

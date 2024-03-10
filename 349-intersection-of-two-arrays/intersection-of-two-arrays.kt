10278a5ea99490c6c3314659b831e288cba797e8
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-03-10 19:03:52 +0200

    Time: 216 ms (36.32%) | Memory: 38.1 MB (72.65%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
@@ -1,9 +1,18 @@
 class Solution {
      fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
-        val intersectSet: HashSet<Int> = nums1.toHashSet()
+        val intersectSet: HashSet<Int> = hashSetOf()
         val result: MutableSet<Int> = hashSetOf()
         var x = 0
-        var y = nums2.lastIndex
+        var y = nums1.lastIndex
+        while (x <= y) {
+            intersectSet.add(nums1[x])
+            intersectSet.add(nums1[y])
+            x++
+            y--
+        }
+
+        x = 0
+        y = nums2.lastIndex
         while (x <= y) {
             if (intersectSet.contains(nums2[x])) {
                 result.add(nums2[x])

735f1844e45b40a83832e29ca5684746d2ac596e
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-03-10 18:53:25 +0200

    Time: 216 ms (36.32%) | Memory: 39.4 MB (9.42%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
@@ -3,21 +3,17 @@ class Solution {
     fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
         var x = 0
         var firstN = true
-        val n1ToSet = nums1.toSet()
-        val n2ToSet = nums2.toSet()
-
-        var y = if (n1ToSet.size >= n2ToSet.size) (n1ToSet.size - 1) else {
+        var y = if (nums1.size >= nums2.size) nums1.lastIndex else {
             firstN = false
-            (n2ToSet.size - 1)
+            nums2.lastIndex
         }
-
         while (x <= y) {
             if (firstN) {
-                checkIsN2(n1ToSet.elementAt(x), n2ToSet)
-                checkIsN2(n1ToSet.elementAt(y), n2ToSet)
+                checkIsN2(nums1[x], nums2)
+                checkIsN2(nums1[y], nums2)
             } else {
-                checkIsN2(n2ToSet.elementAt(x), n1ToSet)
-                checkIsN2(n2ToSet.elementAt(y), n1ToSet)
+                checkIsN2(nums2[x], nums1)
+                checkIsN2(nums2[y], nums1)
             }
             x++
             y--
@@ -25,11 +21,11 @@ class Solution {
         return setOfResult.toIntArray()
     }
 
-    private fun checkIsN2(n: Int, nums2: Set<Int>) {
+    private fun checkIsN2(n: Int, nums2: IntArray) {
         if (n !in setOfResult) {
             var rightIndex = 0
-            while (rightIndex < nums2.size) {
-                if (nums2.elementAt(rightIndex)== n) {
+            while (rightIndex < nums2.size ) {
+                if (nums2[rightIndex]==n){
                     setOfResult.add(n)
                     break
                 }

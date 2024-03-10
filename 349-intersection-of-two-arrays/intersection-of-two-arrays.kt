db31300f0a7ddd9c3d3ec2583bfa206c3ef39721
Ali Tarek Hrhera <alihrhera1@gmail.com>
2024-03-10 19:00:48 +0200

    Time: 214 ms (43.05%) | Memory: 39.4 MB (9.42%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
@@ -1,40 +1,20 @@
 class Solution {
-   val setOfResult = mutableSetOf<Int>()
-    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+     fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val intersectSet: HashSet<Int> = nums1.toHashSet()
+        val result: MutableSet<Int> = hashSetOf()
         var x = 0
-        var firstN = true
-        val n1ToSet = nums1.toHashSet()
-        val n2ToSet = nums2.toHashSet()
-
-        var y = if (n1ToSet.size >= n2ToSet.size) (n1ToSet.size - 1) else {
-            firstN = false
-            (n2ToSet.size - 1)
-        }
-
+        var y = nums2.lastIndex
         while (x <= y) {
-            if (firstN) {
-                checkIsN2(n1ToSet.elementAt(x), n2ToSet)
-                checkIsN2(n1ToSet.elementAt(y), n2ToSet)
-            } else {
-                checkIsN2(n2ToSet.elementAt(x), n1ToSet)
-                checkIsN2(n2ToSet.elementAt(y), n1ToSet)
+            if (intersectSet.contains(nums2[x])) {
+                result.add(nums2[x])
+            }
+            if (intersectSet.contains(nums2[y])) {
+                result.add(nums2[y])
             }
             x++
             y--
         }
-        return setOfResult.toIntArray()
-    }
 
-    private fun checkIsN2(n: Int, nums2: HashSet<Int>) {
-        if (n !in setOfResult) {
-            var rightIndex = 0
-            while (rightIndex < nums2.size) {
-                if (nums2.elementAt(rightIndex)== n) {
-                    setOfResult.add(n)
-                    break
-                }
-                rightIndex++
-            }
-        }
+        return result.toIntArray()
     }
 }
